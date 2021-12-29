19c9172ccd653646c053fbb9667113e20b7f015b
Eric <43848365+eegli@users.noreply.github.com>
2021-12-29 21:05:40 +0100

    wip

--- test/index.test.ts
+++ test/index.test.ts
@@ -8,23 +8,28 @@ jest.useFakeTimers('modern').setSystemTime(new Date(1996, 3, 21));
 
 const mockId = '1234';
 
-const spies = {
-  id: jest.spyOn(utils, 'id').mockReturnValue(mockId),
-  writeFile: jest.spyOn(fs, 'writeFileSync').mockImplementation(jest.fn),
-  consoleInfo: jest.spyOn(global.console, 'info').mockImplementation(jest.fn),
-  localhost: jest.spyOn(request, 'getLocalhostUrl').mockImplementation(() => {
+jest.spyOn(utils, 'id').mockReturnValue(mockId);
+const writeFileSpy = jest
+  .spyOn(fs, 'writeFileSync')
+  .mockImplementation(jest.fn);
+const consoleInfoSpy = jest
+  .spyOn(global.console, 'info')
+  .mockImplementation(jest.fn);
+const localhostSpy = jest
+  .spyOn(request, 'getLocalhostUrl')
+  .mockImplementation(() => {
     return Promise.resolve(`?code=AQDKHwNyRapw&state=${mockId}`);
-  }),
-  request: jest.spyOn(request, 'request').mockImplementation(() => {
-    return Promise.resolve({
-      access_token: 'BQC2fMYf9',
-      token_type: 'Bearer',
-      expires_in: 3600,
-      refresh_token: 'OFuVRQAzVzq0Wy_Py_W4KiMr8H0',
-      scope: 'user-library-read',
-    });
-  }),
-};
+  });
+const requestSpy = jest.spyOn(request, 'request').mockImplementation(() => {
+  return Promise.resolve({
+    access_token: 'BQC2fMYf9',
+    token_type: 'Bearer',
+    expires_in: 3600,
+    refresh_token: 'OFuVRQAzVzq0Wy_Py_W4KiMr8H0',
+    scope: 'user-library-read',
+  });
+});
+
 afterEach(() => {
   jest.clearAllMocks();
 });
@@ -60,18 +65,18 @@ describe('App', () => {
   testConfigs.forEach((config, idx) => {
     it(`works with config ${idx}`, async () => {
       await auth(config);
-      expect(spies.consoleInfo.mock.calls[1][0]).toMatchSnapshot(
+      expect(consoleInfoSpy.mock.calls[1][0]).toMatchSnapshot(
         `config ${idx}, auth url`
       );
-      expect(spies.localhost).toHaveBeenCalledWith(config.port);
-      expect(spies.request.mock.calls[0][0]).toMatchSnapshot(
+      expect(localhostSpy).toHaveBeenCalledWith(config.port);
+      expect(requestSpy.mock.calls[0][0]).toMatchSnapshot(
         `config ${idx}, spotify request`
       );
-      expect(spies.writeFile.mock.calls[0][0]).toMatchSnapshot(
+      expect(writeFileSpy.mock.calls[0][0]).toMatchSnapshot(
         `config ${idx}, out dir`
       );
       expect(
-        JSON.parse(spies.writeFile.mock.calls[0][1] as string)
+        JSON.parse(writeFileSpy.mock.calls[0][1] as string)
       ).toMatchSnapshot(`config ${idx}, written data`);
     });
   });
